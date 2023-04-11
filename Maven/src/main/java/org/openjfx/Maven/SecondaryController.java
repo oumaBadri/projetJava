0b280b225910f17d5178e654ae5079c37815cfef
dhia <dhia@LAPTOP-OGAIF04A>
2023-04-11 10:27:03 +0000

    syso

--- Maven/src/main/java/org/openjfx/Maven/SecondaryController.java
+++ Maven/src/main/java/org/openjfx/Maven/SecondaryController.java
@@ -8,5 +8,6 @@ public class SecondaryController {
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+        System.out.println("hello");
     }
 }
