77b2deafc885c47c360dce182f22457d3a6e108f
dhia <dhia@LAPTOP-OGAIF04A>
2023-04-12 23:35:13 +0000

    ConsultationController

--- Maven/src/main/java/module-info.java
+++ Maven/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module org.openjfx.Maven {
     requires javafx.controls;
     requires javafx.fxml;
 	requires java.sql;
+	requires javafx.graphics;
+	requires javafx.base;
 
     opens org.openjfx.Maven to javafx.fxml;
     exports org.openjfx.Maven;
