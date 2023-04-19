e437612a687adeb72f3ebacf1509b0dd4ffcf72d
PREDATOR <PREDATOR@DESKTOP-89I2J2U>
2023-04-19 04:41:55 +0100

    y

--- Maven/src/main/java/org/openjfx/Maven/App.java
+++ Maven/src/main/java/org/openjfx/Maven/App.java
@@ -36,30 +36,23 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-<<<<<<< HEAD
-=======
 
-        scene = new Scene(loadFXML("AjoutShow"),1000,790);
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
 
-<<<<<<< HEAD
+       // scene = new Scene(loadFXML("AjoutShow"),1000,790);
+
        // scene = new Scene(loadFXML("AjoutShow"),1000,790);
 
 
         scene = new Scene(loadFXML("login"),1000,790);
-=======
+
        // scene = new Scene(loadFXML("login"),1000,790);
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
 
        // scene = new Scene(loadFXML("ProfileInterface"),1000,790);
 
       //  scene = new Scene(loadFXML("ProfileInterface"),1000,790);
 
        // scene = new Scene(loadFXML("UserHome"),1000,790);
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
         stage.setScene(scene);
         stage.show();
     }
@@ -72,41 +65,24 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-<<<<<<< HEAD
 
-
-=======
     public static void main(String[] args) throws SQLException {
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
 
 
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
  //System.out.println(AvisDAO.ShowTitre(1));
-    public static void main(String[] args) throws ParseException, SQLException {
+    
 
-<<<<<<< HEAD
 //System.out.println(AvisDAO.envoyerNotif(1));
     launch();
     	/*System.out.println(AvisDAO.idTitre("the originals"));
     	System.out.println(AvisDAO.findAvis(1, 1).toString2());
     	System.out.println(AvisDAO.findAvis(1, 1).getNum_ep());*/
-=======
     	//System.out.println("hello");
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
 
     	/*LocalDate date = LocalDate.of(20,10,20);
     	Show s=new Show(12, "lalala", date, "Tunisia", "arabic","Dramatique", 1, "url de l'image");
     	ShowDAO.ajouterShow(s);*/
 
-<<<<<<< HEAD
-=======
-      launch();
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
     //	System.out.println(AvisDAO.findAll2(1));
     //	System.out.println(AvisDAO.ShowTitre(1));
     	//Avis a= new Avis(1, 1, 10, "jaime", 1, 1);
@@ -135,13 +111,10 @@ public class App extends Application {
         //launch();
 >>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava*/
 
-<<<<<<< HEAD
     	/*Connection cnx = utile.conxBD.getInstance();
     	System.out.println("la connection is "+cnx);*/
 
       //LocalDate date = LocalDate.of(20,10,20);
-=======
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
      /* Utilisateur u = new Utilisateur(20, "ouma", "Badri",date,"badri@gmail.com" ,"12345678","");
 =======
     /*  Utilisateur u = new Utilisateur(20, "hamoudaa", "badrii",date,"hamouda@gamail.com" ,"12345678");
@@ -152,26 +125,15 @@ public class App extends Application {
      /*  Producteur p =new Producteur(20, "heelloo", "baadroo", "badri@gmail", "12345678",date);
         ProducteurDAO.ajouterProducteur(p);*/
         //hello world
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
     /*	List<Show> sh= ShowDAO.findAll2();
     	System.out.println(sh);*/
-<<<<<<< HEAD
 
-=======
-        
-        
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
     	//test
     	/*LocalDate date=LocalDate.of(2002,06, 18);
     	Acteur a=new Acteur(12,"malak","bendhia","bendhia@gmail.com","1236457",date);
     	Dao.ActeurDAO.ajouterActeur(a);*/
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/oumaBadri/projetJava
     }
 
 
